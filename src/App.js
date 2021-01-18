dae92da25859d3e2daf433d5f4ad8f5154b474ca
joaopinheiro <joao.pinheiro@load.digital>
2021-01-18 15:57:09 +0000

    using props to access state in different component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/person';
 
-class App extends Component {
+class App extends Component
+{
 
   state = {
     people: [
@@ -11,36 +12,56 @@ class App extends Component {
       {name: 'Stephanie', age: 26}
     ],
     otherState: 'some other value'
-  }
+  };
 
-  switchNameHandler = () =>
+  switchNameHandler = (newName) =>
   {
     // console.log('Was clicked');
     // this.state.people[0].name = 'João'; --> Not to be done
     this.setState(
       {
         people: [
-          {name: 'João', age: 28},
+          {name: newName, age: 28},
           {name: 'Manu', age: 29},
           {name: 'Stephanie', age: 27}
         ] 
       }
     )
-  }
+  };
+
+  nameChangedHandler = (event) =>
+  {
+    this.setState(
+      {
+        people: [
+          {name: event.target.value, age: 28},
+          {name: 'Manu', age: 29},
+          {name: 'Stephanie', age: 27}
+        ] 
+      }
+    )
+  } 
 
   render() 
   {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
+        <button onClick={() => this.switchNameHandler('Test')}>Switch Name</button>
+        <Person name={this.state.people[0].name} 
+          age={this.state.people[0].age}
+          changed={this.nameChangedHandler}/>
+        <Person name={this.state.people[1].name} 
+          age={this.state.people[1].age}
+          click={this.switchNameHandler.bind(this, 'João!!!')}>
+          My hobbies: Racing
+        </Person>
+        <Person name={this.state.people[2].name} 
+          age={this.state.people[2].age}/>
       </div>
     );
+    }
     // return React.createElement('h1', {className: 'App'}, 'Hi, I\'m a React App!!!!');
-  }
 }
 
 export default App;
