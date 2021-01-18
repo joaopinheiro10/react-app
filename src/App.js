452877113e1279958fce6ea8c074dc08110db918
joaopinheiro <joao.pinheiro@load-interactive.com>
2021-01-18 12:37:07 +0000

    used state and set state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,40 @@ import './App.css';
 import Person from './Person/person';
 
 class App extends Component {
-  render() {
+
+  state = {
+    people: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () =>
+  {
+    // console.log('Was clicked');
+    // this.state.people[0].name = 'João'; --> Not to be done
+    this.setState(
+      {
+        people: [
+          {name: 'João', age: 28},
+          {name: 'Manu', age: 29},
+          {name: 'Stephanie', age: 27}
+        ] 
+      }
+    )
+  }
+
+  render() 
+  {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
       </div>
     );
     // return React.createElement('h1', {className: 'App'}, 'Hi, I\'m a React App!!!!');
--- src/Person/person.js
+++ src/Person/person.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const person = () =>
+const person = (props) =>
 {
-    return <p>I'm a Person!</p>
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
